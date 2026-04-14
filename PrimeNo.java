b903b801872ef775625a734cc246caa2005117ee
lav kumar <lavkumar9529@gmail.com>
2026-04-14 06:56:11 +0530

    Add files via upload

---
+++ PrimeNo.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class PrimeNo{
+    public static boolean isPrime(int n){
+        boolean isPrime = true;
+        for(int i=2; i<=n-1; i++) {
+            if(n%i==0){
+                isPrime = false;
+                break;
+            }
+        }
+        return isPrime;
+    }
+    public static void primeInRange(int n){
+        for(int i=2; i<=n; i++){
+            if(isPrime(i)){
+                System.out.print(i+" ");
+            }
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter N number: ");
+        int n = sc.nextInt();
+        primeInRange( n);
+
+        sc.close();
+    }
+    
+}
+  
+ 
