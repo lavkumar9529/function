53a61d4730df4b5fb8a0096bf3c4ef8096d0ec1e
lav kumar <lavkumar9529@gmail.com>
2026-04-14 06:55:15 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void Hello(){
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+    }
+    public static void main(String[] args) {
+        Hello();
+    }
+}
